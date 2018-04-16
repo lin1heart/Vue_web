1b89c090801c3de7ad7a7ee428e778560f61b5c8
lin1heart <lin1heart@gmail.com>
2018-04-16 18:43:00 +0800

    dopost image fix

--- js/category.js
+++ js/category.js
@@ -1,24 +1,134 @@
 var banner = new Vue({
 	el: '#banner_full',
 	data:{
-		onlineCount: 1,
-		dbCount: 2,
-		selfCount: 3
+		onlineCount: "",
+		dbCount: "",
+		selfCount: ""
 	}
 })
 
+var isClient = /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
+
 $(function(){
-	console.log(1);
-	$.ajax({
-		type:"GET",
-		url:"http://127.0.0.1:8080/user/version",
-		async:true,
-		data: "",
-		success: function(a,b,c){
-			console.log(a,b,c);
-		},
-		error: function(a,b,c){
-			console.log(123);
-		}
-	});
+	doPost('version');
+	doPost("getList",{"pageIndex": "0","pageSize":"10","type":"0"});
+//	doPost("register",{"username":"333","password":"22","mail":"406644209@qq.com"});
+//	doPost('login',{"username":"111","password":"123"});
 })
+var image_lsit = new Vue({
+	el: '#image_container',
+	data: {
+		totalWidth: isClient ? '100%' : '70%',
+		totalLeft: isClient ? 0 : '15%',
+		pic:[]
+	}
+})
+
+alert(image_lsit.totalWidth + image_lsit.totalLeft + isClient);
+/*******************version请求*******************************/
+function versionRequest(request,form){
+	request.head.bid = "user";
+	request.head.fid = "version";
+	request.head.typ = "GET";
+	request.body = {}
+}
+
+function versionResponse(response){
+	if(response.code =="200"){
+		var data = response.data;
+		banner.dbCount = data.dbCount;
+		banner.onlineCount = data.onlineCount;
+		banner.selfCount = data.selfCount;
+	}else{
+		console.log("version Response:"+getMessage(response));
+	}
+}
+
+function versionException(exception, code, status){
+	console.log("version 请求发送失败:"+exception, code, status);
+}
+
+/*******************登录请求*******************************/
+function loginRequest(request,form){
+	request.head.bid = "user";
+	request.head.fid = "login";
+	request.head.typ = "POST";
+	request.body = {
+		username: form.username,
+		password: form.password
+	}
+}
+
+function loginResponse(response){
+	if(response.code =="200"){
+		console.log("login:"+response);
+		
+	}else{
+		console.log("login Response" + getMessage(response));
+	}
+}
+
+function loginException(exception, code, status){
+	console.log("login 请求发送失败:"+exception, code, status);
+}
+
+/*******************注册请求*******************************/
+function registerRequest(request,form){
+	request.head.bid = "user";
+	request.head.fid = "register";
+	request.head.typ = "POST";
+	request.body = {
+		username: form.username,
+		password: form.password,
+		mail: form.mail
+	}
+}
+
+function registerResponse(response){
+	if(response.code =="200"){
+		console.log("register:"+response);
+		
+	}else{
+		console.log("register Response" + getMessage(response));
+	}
+}
+
+function registerException(exception, code, status){
+	console.log("register 请求发送失败:"+exception, code, status);
+}
+
+/*******************imageList请求*******************************/
+function getListRequest(request,form){
+	request.head.bid = "image";
+	request.head.fid = "getList";
+	request.head.typ = "GET";
+	request.body = {
+		pageIndex: form.pageIndex,
+		pageSize: form.pageSize,
+		type: form.type
+	}
+}
+
+var IMAGE_URL = 'http://13.250.226.195:8888/dbImage/';
+
+function getListResponse(response){
+	if(response.code =="200"){
+		var imageList = [];
+		for(var i = 0; i< response.data.length; i++){
+			var aspectRatio = response.data[i].width/response.data[i].height;
+			image_lsit.$data.pic.push({
+				aspectRatioWidth: aspectRatio*200 + 'px',
+				aspectRatioFlex: aspectRatio*200,
+				ipadding: 1/aspectRatio * 100 + '%',
+				imgurl: IMAGE_URL + response.data[i].headImage,
+				title: response.data[i].title
+			})
+		}
+	}else{
+		console.log("getList Response" + getMessage(response));
+	}
+}
+
+function getListException(exception, code, status){
+	console.log("getList 请求发送失败:"+exception, code, status);
+}
---
+++ js/common.js
@@ -0,0 +1,56 @@
+function doPost(event, data) {
+	event = event || {};
+	data = data || {};
+
+	var request = {
+		head: {},
+		body: {}
+	};
+
+	invokeEventMethod(event, "Request", [request, data]);
+	var head =  request.head || { bid: "", fid: "", typ: "GET"};
+	var bid = "/" + head.bid;
+	var fid = "/" + head.fid;
+	var type = head.typ;
+	var da;
+//	if(type.toUpperCase() =="GET"){
+		da = request.body;		
+//	}else if(type.toUpperCase() =="POST"){
+//		da = JSON.stringify(request.body);		
+//	}
+	if(da == "{}"){
+		da = "";
+	}
+	$.ajax({
+		url: POST_URL + bid + fid,
+		type: type,
+		async:true,
+		contentType: 'application/x-www-form-urlencoded',
+		data: da,
+//		dataType: "json",
+		success: function(response, status, xhr) {
+			invokeEventMethod(event, "Response", [response, data]);
+		},
+		error: function(xhr, status, exception) {
+			invokeEventMethod(event, "Exception", [exception, xhr.status, status]);
+		}
+	});
+}
+//var POST_URL = "http://127.0.0.1:8080";
+var POST_URL = "http://13.250.226.195:8080";
+
+function invokeEventMethod(event, method, arguments, otherwise) {
+	if(window[event + method] instanceof Function) {
+		return window[event + method].apply(this, arguments);
+	} else {
+		return otherwise === undefined ? true : otherwise;
+	}
+}
+
+function getMessage(response) {
+	var message = response.msg;
+	if(message) {
+		message = "（" + message + "）";
+	}
+	return message;
+}
